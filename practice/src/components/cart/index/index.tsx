e82cf0e1a824f2e1d8ae017a37730e46bd8ca66e
mayun1203 <m.uchida@riot-company.com>
2024-10-08 19:07:04 +0900

    commit:editに住所選択途中

--- practice/src/components/cart/index/index.tsx
+++ practice/src/components/cart/index/index.tsx
@@ -1,8 +1,15 @@
 "use client"
 import React, { useEffect, useState } from "react";
 
+type PrefectureType = {
+    prefCode:number,
+    prefName:string
+}
 export function SampleFetch () {
-    const [task, setTask] = useState(null);
+    // const [tasks, setTasks] = useState([]);
+    // const [selectTask, setSelectTask] = useState("");
+    const [prefectures, setPrefectures] = useState<PrefectureType[]>([]);
+    const [selectedPrefecture, setSelectedPrefecture] = useState("");
 
     useEffect(() => {
         fetch('https://opendata.resas-portal.go.jp/api/v1/prefectures', {
@@ -14,24 +21,43 @@ export function SampleFetch () {
         // レスポンスをJSONに変換
         .then(response => response.json())
         // 取得したデータをtaskに保存
-        .then(data =>setTask(data))
+        .then(data =>setPrefectures(data.result))
         .catch(error => console.error("Fetch data failed", error));
     }, []);
 
-    if (!task) return <div>Loading...</div>;
+    const selectChange = (event:React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedPrefecture(event.target.value);
+    };
+
+    if (!prefectures) return <div>Loading...</div>;
 
     return(
         <div>
             <h1>Task Information</h1>
-            <p className="container" key={task.id}>
-            {task.result.map((result) => {
-                return (
-                    <div className="task" key={task.id}>
-                        <p>{result.prefCode}:{result.prefName}</p>
-                    </div>
-                );
-            })}
-            </p>
+            <select value={selectedPrefecture} onChange={selectChange}>
+                <option value="">選択して下さい</option>
+                {prefectures.map((prefecture, index) => {
+                    console.log(prefecture, "aaa")
+                    return (
+                    <option key={index}>
+                        {prefecture.prefName}
+                    </option>
+                    )
+                })}
+            </select>
+            {/* 選択した都道府県の表示 */}
+            {selectedPrefecture && <p>選択された都道府県： {selectedPrefecture}</p>}
         </div>
-    )
-};
+    );
+}
+
+{/* ただただAPIをそのまま出すコード */}
+            // {/* <p className="container" key={task.id}>
+            // {task.result.map((result) => {
+            //     return (
+            //         <div className="task" key={task.id}>
+            //             <p>{result.prefCode}:{result.prefName}</p>
+            //         </div>
+            //     );
+            // })}
+            // </p>
